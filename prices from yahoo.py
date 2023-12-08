c82e79acebd90f098eef093420eb5f4690a4557b
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-12-08 23:16:52 +0500

    Update prices from yahoo.py
    Added option to get for several stocks

--- prices from yahoo.py
+++ prices from yahoo.py
@@ -1,9 +1,29 @@
 import yfinance as yf
+import pandas as pd
 
-def prices_yahoo(x, s, e):
-  
-  new_df = pd.DataFrame(yf.Ticker(x).history(start=s, end=e)).iloc[:,3]
-  
-  print(new_df)
-  
-prices_yahoo("AIG", "2010-05-31", "2021-01-30")
+def prices_yahoo(y, s=None, e=None):
+    p = pd.DataFrame()  # Create an empty DataFrame
+
+    # Loop for data extraction & Set up statements for start and end dates
+    for ticker in y:
+        if s is None and e is None:
+            # When neither start date nor end date is defined
+            data = yf.download(ticker)
+        elif e is None:
+            data = yf.download(ticker, start=s) # Only start date is defined
+        elif s is None:
+            data = yf.download(ticker, end=e)  # When only end date is defined
+        else:
+            # When both start date and end date are defined
+            data = yf.download(ticker, start=s, end=e)
+
+        # Extract the Adjusted Close prices and add to the DataFrame
+        if not data.empty:
+            p[ticker] = data['Adj Close']
+
+    p = p.dropna() # Drop rows with NA values
+
+    return p
+
+result = prices_yahoo(y=["UNM", "AIG", "OMF", "MET", "HIG"], s="2022-01-01")
+print(result) # Test
