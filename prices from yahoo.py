e79f7cad15662ed48cb8969d35dc36dad55ee826
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-12-08 22:49:20 +0500

    Update prices from yahoo.py
    Upgraded to show only close column

--- prices from yahoo.py
+++ prices from yahoo.py
@@ -2,6 +2,8 @@ import yfinance as yf
 
 def prices_yahoo(x, s, e):
   
-  print(yf.Ticker(x).history(start=s, end=e))
+  new_df = pd.DataFrame(yf.Ticker(x).history(start=s, end=e)).iloc[:,3]
+  
+  print(new_df)
   
 prices_yahoo("AIG", "2010-05-31", "2021-01-30")
